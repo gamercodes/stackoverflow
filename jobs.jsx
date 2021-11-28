a7b0659c3da2ffcfa05b7c83bf3c5312eb4e487e
gamercodes <95175677+gamercodes@users.noreply.github.com>
2021-11-28 19:31:33 +0530

    Create jobs.jsx

---
+++ jobs.jsx
@@ -0,0 +1,51 @@
+const state = useContext(GlobalState)
+const[jobs,setJobs]=state.categoriesAPI.jobs
+useEffect( ()=>{
+
+const  getmyUser=async ()=>{
+
+         const {data:user}= await http.get(MyApiUrl+'/api/users/job')
+         setUser(user)
+        
+        }
+     
+        const     populateJobs=async()=>{
+         try{
+         
+           
+         
+             
+               
+               
+               const myjobs=[...jobs];
+               
+               for(let i=0;i<myjobs.length;i++){
+                 if(_.includes(user.saved,myjobs[i]._id)){
+                 
+                   myjobs[i].liked=true; // I WANT THIS CHANGE TO REFLECT IN RENDER fn() .
+                 }
+               }
+              setJobs(myjobs)
+     
+     
+         
+               }
+              catch(ex){
+                  console.log('hey',ex)
+              } 
+         
+             
+             }
+
+               getmyUser()
+               populateJobs();
+  
+
+
+       
+
+ 
+ },[])
+
+render({ <Card jobs={jobs} className="cardsjob" ondelete={handleDelete} onLike={handleLike} user={user}/>})
+
